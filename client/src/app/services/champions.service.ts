5b1323984dfc5453636a521a05b3e32b6eceea93
Rion <PF66FBd@ad.ing.net>
2019-09-12 17:53:54 +0200

    setup client missing styles, and should improve load more func

--- client/src/app/services/champions.service.ts
+++ client/src/app/services/champions.service.ts
@@ -4,23 +4,32 @@ import { HttpClient } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 
-import { Champion } from "./champion";
+import { Champion } from "../model/champion";
+import championList from "../../../../server/data/champions.json";
 
 @Injectable({
   providedIn: "root"
 })
-export class ChampionsListService {
+export class ChampionsService {
   private championsUrl = "api/champions";
 
   constructor(private http: HttpClient) {}
 
   getChampionsList(): Observable<Champion[]> {
-    return this.http.get<Champion[]>(this.championsUrl).pipe(
+    /*return this.http.get<Champion[]>(this.championsUrl).pipe(
       tap(result => this.log(JSON.stringify(result))),
       catchError(this.handleError<Champion[]>("getChampionsList", []))
-    );
+    );*/
+    return of(championList);
   }
 
+  getChampion(id): Observable<Champion> {
+    /*return this.http.get<Champion>(this.championsUrl + "/" + id).pipe(
+      tap(result => this.log(JSON.stringify(result))),
+      catchError(this.handleError<Champion>("getChampion", []))
+    );*/
+    return of(championList.find(champion => champion.id === id));
+  }
   private log(message: string) {
     console.log(`ChampionsListService: ${message}`);
   }
